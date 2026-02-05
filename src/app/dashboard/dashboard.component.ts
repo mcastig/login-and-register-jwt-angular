9776d6c2f6fdcab736dcde60831d74ea90515994
Maximino Castillo <maxcastgarcia@gmail.com>
2026-02-05 13:39:23 -0600

    Implement dasboard section

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-dashboard.component',
   imports: [],
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent { }
+export class DashboardComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  // Getting the Signal from the service
+  user = this.authService.currentUser;
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
